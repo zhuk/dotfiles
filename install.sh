71d558f4b1b97b48ece434ebbd7eedf4a6b464f3
zhux <zhuknir@gmail.com>
2023-05-04 17:02:23 +0800

    fix link name issue

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # vim
-ln -s ${BASEDIR}/lazyvim.starter ~/.config/nvim
+ln -s -T ${BASEDIR}/lazyvim.starter ~/.config/nvim
 
 # zsh
 ln -s ${BASEDIR}/zshrc ~/.zshrc
